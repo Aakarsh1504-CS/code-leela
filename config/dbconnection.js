1036916e1720f0e51d03d0aa36e9456def92c2fa
Aakarsh Arora <aroraaakarsh0@gmail.com>
2024-06-25 15:39:04 +0530

    Update dbconnection.js

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/insta-test");
-// mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect("mongodb://127.0.0.1:27017/insta-test");
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });

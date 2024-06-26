116ad7185c903981ee724cf45dd0027b40ce5b77
Aakarsh Arora <aroraaakarsh0@gmail.com>
2024-06-26 12:24:12 +0530

    Update multer-config.js

--- config/multer-config.js
+++ config/multer-config.js
@@ -1,4 +1,21 @@
-const multer=require('multer');
-const storage=multer.memoryStorage();
-const upload=multer({storage});
-module.exports=upload;
+// multer-config.js
+
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/jpg') {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only JPEG, JPG, and PNG are allowed!'), false);
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+    limits: { fileSize: 1024 * 1024 * 5 } // 5MB file size limit
+});
+
+module.exports = upload;
